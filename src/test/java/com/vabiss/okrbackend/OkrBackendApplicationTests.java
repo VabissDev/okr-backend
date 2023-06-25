240c32299743dae3c3ac73232cea49acfbaa4862
vusalaxndzde <vusal.axndzde@gmail.com>
2023-06-26 03:51:19 +0400

    Initial commit

---
+++ src/test/java/com/vabiss/okrbackend/OkrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vabiss.okrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
