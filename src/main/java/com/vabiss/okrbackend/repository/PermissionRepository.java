29c629ef147dff5471f9c16a34632f809a526a5c
vusalaxndzde <vusal.axndzde@gmail.com>
2023-07-01 21:46:34 +0400

    permission repository

--- src/main/java/com/vabiss/okrbackend/repository/PermissionRepository.java
+++ src/main/java/com/vabiss/okrbackend/repository/PermissionRepository.java
@@ -2,11 +2,8 @@ package com.vabiss.okrbackend.repository;
 
 import com.vabiss.okrbackend.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Integer> {
-
 
 
 }
