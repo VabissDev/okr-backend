a36883a24e421b1e8d43fe2d96061c92f48cfd4e
Javidan Niyazali <cavidan.niyazali@gmail.com>
2023-07-03 14:04:38 +0400

    added entity Workspace and liquibase files

---
+++ src/main/java/com/vabiss/okrbackend/entity/Workspace.java
@@ -0,0 +1,43 @@
+package com.vabiss.okrbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Entity
+@Table(name = "workspaces")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Data
+@Builder
+@AllArgsConstructor
+public class Workspace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "workspace_id", nullable = false, updatable = false, unique = true)
+    int workspaceId;
+
+    @Column(name = "workspace_name", nullable = false)
+    String workspaceName;
+
+    @Column(name = "owner", nullable = false)
+    String owner;
+
+    @Column(name = "description", nullable = false)
+    String description;
+
+    @Column(name = "level", nullable = false)
+    String level;
+
+    @Column(name = "status", nullable = false)
+    String status;
+
+    @ManyToMany(mappedBy = "workspaces", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    List<User> users;
+
+    @ManyToOne
+    @JoinColumn(name = "organization_id")
+    Organization organization;
+}
---
+++ src/test/java/com/vabiss/okrbackend/OkrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vabiss.okrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrBackendApplication.class, args);
+	}
+
+}
