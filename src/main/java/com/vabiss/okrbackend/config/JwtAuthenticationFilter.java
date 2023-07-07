8b4ccd76ddbcf2bc909bf3af0f9d6a778ad58c17
vusalaxndzde <vusal.axndzde@gmail.com>
2023-07-07 14:51:15 +0400

    Create jwtService

--- src/main/java/com/vabiss/okrbackend/config/JwtAuthenticationFilter.java
+++ src/main/java/com/vabiss/okrbackend/config/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -24,7 +25,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+                    @NonNull HttpServletRequest request,
+                    @NonNull HttpServletResponse response,
+                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
         final String header = request.getHeader("Authentication");
         final String jwt;
         final String username;
